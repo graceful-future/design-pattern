cacab91c7d882735d05a62b8cb5870b1ba14e480
chenkun <chenkun@zcreate.com.cn>
2021-05-06 16:02:44 +0800

    单例模式测试

--- src/main/java/com/gracefulfuture/design/pattern/singleton/SingeltonTest.java
+++ src/main/java/com/gracefulfuture/design/pattern/singleton/SingeltonTest.java
@@ -1,24 +1,17 @@
 package com.gracefulfuture.design.pattern.singleton;
 
 
-import com.gracefulfuture.design.pattern.singleton.hungry.HungrySingleton;
 import com.gracefulfuture.design.pattern.singleton.sic.SICSingleton;
 
 public class SingeltonTest {
     public static void main(String[] args){
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    SICSingleton sicSingleton = SICSingleton.getInstance();
-                    System.out.println(sicSingleton);
-                }
+            new Thread(() -> {
+                SICSingleton sicSingleton = SICSingleton.getInstance();
+                System.out.println(sicSingleton);
             }).start();
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    SICSingleton sicSingleton = SICSingleton.getInstance();
-                    System.out.println(sicSingleton);
-                }
+            new Thread(() -> {
+                SICSingleton sicSingleton = SICSingleton.getInstance();
+                System.out.println(sicSingleton);
             }).start();
     }
 }
