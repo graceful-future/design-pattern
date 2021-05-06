923160559497f2e4584af9c5f158cae153cb36a2
chenkun <chenkun@zcreate.com.cn>
2021-05-06 14:37:06 +0800

    设计模式-单例模式(懒汉式)

---
+++ src/main/java/com/gracefulfuture/design/pattern/singleton/lazy/LazySingleton.java
@@ -0,0 +1,15 @@
+package com.gracefulfuture.design.pattern.singleton.lazy;
+
+//懒汉式
+public class LazySingleton {
+    private static LazySingleton lazySingleton = null;
+    private LazySingleton(){
+
+    }
+    public static LazySingleton getInstance(){
+        if(lazySingleton == null){
+            lazySingleton = new LazySingleton();
+        }
+        return lazySingleton;
+    }
+}
